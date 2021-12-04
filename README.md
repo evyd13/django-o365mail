d086c3d07d1582b9d8656694e4399c746d84aa99
Evelien Dekkers <sixmoonskies@gmail.com>
2021-12-04 17:13:46 +0100

    wip mailbox connection pt2

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Example Package
+# django-o365mail
 
-This is a simple example package. You can use
-[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
-to write your content.
+Readme goes here
--- src/django_o365mail/backend.py
+++ src/django_o365mail/backend.py
@@ -1,6 +1,8 @@
-from django.core.mail.backends.base import BaseEmailBackend
-import O365
+import threading
 
+from django.core.mail.backends.base import BaseEmailBackend
+from django.conf import settings
+import O365
 
 """
 A wrapper that manages the O365 API for sending emails.
@@ -9,9 +11,51 @@ See https://docs.microsoft.com/en-us/graph/auth-v2-service?context=graph%2Fapi%2
 """
 
 class O365EmailBackend(BaseEmailBackend):
+    def __init__(self, client_id=None, client_secret=None, tenant_id=None,
+                 fail_silently=False, **kwargs):
+        super().__init__(fail_silently=fail_silently)
+        self.client_id = client_id or settings.O365_MAIL_CLIENT_ID
+        self.client_secret = client_secret or settings.O365_MAIL_CLIENT_SECRET
+        self.tenant_id = tenant_id or settings.O365_MAIL_TENANT_ID
+
+        self.mailbox = None
+        self._lock = threading.RLock()
+
     def open(self):
-        pass
+        """
+        Ensure an open connection to the email server. Return whether or not a
+        new connection was required (True or False) or None if an exception
+        passed silently.
+        """
+        if self.mailbox:
+            # Nothing to do if the mailbox is already open.
+            return False
+
+        credentials = (self.client_id, self.client_secret)
+        account = O365.Account(credentials, auth_flow_type='credentials', tenant_id=self.tenant_id)
+        try:
+            if account.authenticate():
+                self.mailbox = account.mailbox()
+                return True
+        except:
+            if not self.fail_silently:
+                raise
+
     def close(self):
         pass
+
     def send_messages(self, email_messages):
-        pass
+        if not email_messages:
+            return 0
+        with self._lock:
+            new_mailbox_created = self.open()
+            if not self.mailbox or new_mailbox_created is None:
+                return 0
+            num_sent = 0
+            for message in email_messages:
+                sent = self._send(message)
+                if sent:
+                    num_sent += 1
+            if new_mailbox_created:
+                self.close()
+        return num_sent
