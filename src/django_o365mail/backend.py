78529f9d757ca9540cfc2d642e3cee42b9d76f8a
Evelien Dekkers <sixmoonskies@gmail.com>
2021-12-04 16:31:09 +0100

    First upload

---
+++ src/django_o365mail/backend.py
@@ -0,0 +1,16 @@
+from django.core.mail.backends.base import BaseEmailBackend
+import O365
+
+
+"""
+A wrapper that manages the O365 API for sending emails.
+Uses an identity (auth_flow_type == 'credentials').
+See https://docs.microsoft.com/en-us/graph/auth-v2-service?context=graph%2Fapi%2F1.0&view=graph-rest-1.0 for more details.
+"""
+class O365EmailBackend(BaseEmailBackend):
+    def open(self):
+        pass
+    def close(self):
+        pass
+    def send_messages(self, email_messages):
+        pass
