80f8f44ce417054eac6b4ed5a1e38cce568a1ea4
Evelien Dekkers <sixmoonskies@gmail.com>
2021-12-12 13:26:46 +0100

    Upload files

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = str(BASE_DIR) + '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
